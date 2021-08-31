932eab7da524f6f0362a9fad1794e94d00d39eeb
Ruslan <rurusich.2001@gmail.com>
2021-08-31 10:04:11 +0400

    added movie counts and rate

--- js/script.js
+++ js/script.js
@@ -1,21 +1,19 @@
 let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
 
+
+
 let personalMovieDB = {
     count: numberOfFilms,
-    movies: {
-
-    },
-    actors: {
-
-    },
-    genres: {
-
-    },
-    privat: false,
-    
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false, 
 };
 
-let lastMovie = prompt("Последний просмотренный вами фильм?");
-let lastMoveRate = +prompt("На сколько оцените этот фильм?");
+let a = prompt("Последний просмотренный вами фильм?");
+let b = +prompt("На сколько оцените этот фильм?");
+let c = prompt("Последний просмотренный вами фильм?");
+let d = +prompt("На сколько оцените этот фильм?");
 
-personalMovieDB.movies.lastMovie.push(lastMoveRate);
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
